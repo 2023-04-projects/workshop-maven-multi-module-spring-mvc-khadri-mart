809137449c231919914e05e98bb10b0e95640a44
manvithagali52 <manvithag52@gmail.com>
2025-04-04 16:44:18 +0530

    used lombok

--- spring-backend-app/src/main/java/com/khadri/spring/mvc/clothes/form/ClothesForm.java
+++ spring-backend-app/src/main/java/com/khadri/spring/mvc/clothes/form/ClothesForm.java
@@ -1,43 +1,19 @@
 package com.khadri.spring.mvc.clothes.form;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ClothesForm {
 	private String itemName;
 	private int itemQty;
 	private Double itemPrice;
-
-	public ClothesForm() {
-		super();
-	}
-
-	public ClothesForm(String itemName, int itemQty, Double itemPrice) {
-		super();
-		this.itemName = itemName;
-		this.itemQty = itemQty;
-		this.itemPrice = itemPrice;
-	}
-
-	public String getItemName() {
-		return itemName;
-	}
-
-	public int getItemQty() {
-		return itemQty;
-	}
-
-	public Double getItemPrice() {
-		return itemPrice;
-	}
-
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
-
-	public void setItemQty(int itemQty) {
-		this.itemQty = itemQty;
-	}
-
-	public void setItemPrice(Double itemPrice) {
-		this.itemPrice = itemPrice;
-	}
 
 }
