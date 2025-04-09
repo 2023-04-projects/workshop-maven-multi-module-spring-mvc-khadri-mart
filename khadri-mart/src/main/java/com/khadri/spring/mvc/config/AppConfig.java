7cb8f65879c7e77c7272c1a756d50013d1c1e108
sgreddy123 <151907854+sgreddy123@users.noreply.github.com>
2025-04-09 22:16:19 +0530

    used @EnableWebMvc in Appconfig

--- khadri-mart/src/main/java/com/khadri/spring/mvc/config/AppConfig.java
+++ khadri-mart/src/main/java/com/khadri/spring/mvc/config/AppConfig.java
@@ -1,21 +1,27 @@
 package com.khadri.spring.mvc.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = { "com.khadri.spring.mvc" })
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
-	@Bean
-	public ViewResolver internalResourceViewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
-		return resolver;
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/views/", ".jsp");
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/grosary/add/page").setViewName("grosary-add");
+		registry.addViewController("/grosary/modify/page").setViewName("grosary-modify-search");
+		registry.addViewController("/grosary/delete/page").setViewName("grosary-delete");
 	}
 
 }
